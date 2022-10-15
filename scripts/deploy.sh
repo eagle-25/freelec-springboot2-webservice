e328ba5cada87478a2de4c09788a181c6c3f3af8
eagle-25 <seungjun.park025@gmail.com>
2022-10-15 22:13:41 +0900

    chore(cicd): add a deployment script
    add a deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=freelec-springboot2-webservice
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid:" $CURRENT_PID
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java-jar \
+    -dpring.config.location=classpath:/application.properties,classpath:/application-real.,/home/ec2-user/app/application-oauth.properties,/home/ec2-/app/application-real-db.pees \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 & # nohup 실행 시 codedeploy는 무한 대기한다.
+    #이 이슈를 해겨하기 위해 nohup.out 파일을 표준 입출력 용도로 별도로 사용한다.
+    # 이렇게 하지 않을 경우, codedeploy 로그에 표준 입출력이 출력된다. nohup.out 파일이 생기지 않는다.
+    # nohup.out이 끝나기 전까지 codedeploy도 끝나지 않으니, 꼭 이렇게 해야한다.
