6a75c8b5e8ec309da2f2ee133f392744366678ea
eagle-25 <seungjun.park025@gmail.com>
2022-10-18 00:17:51 +0900

    feat(posts): enable author autosaving
    enable author autosaving with user's email.

--- src/main/java/com/eagle25/practice/springboot/web/PostAPIController.java
+++ src/main/java/com/eagle25/practice/springboot/web/PostAPIController.java
@@ -1,11 +1,16 @@
 package com.eagle25.practice.springboot.web;
 
+import com.eagle25.practice.springboot.config.auth.LoginUser;
+import com.eagle25.practice.springboot.config.auth.dto.SessionUser;
 import com.eagle25.practice.springboot.service.posts.PostsService;
 import com.eagle25.practice.springboot.web.dto.PostsResponseDTO;
 import com.eagle25.practice.springboot.web.dto.PostsSaveRequestDTO;
 import com.eagle25.practice.springboot.web.dto.PostsUpdateRequestDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,8 +19,15 @@ public class PostAPIController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDTO requestDTO) {
-        return postsService.save(requestDTO);
+    public Long save(@RequestBody Map<String, Object> req, @LoginUser SessionUser user) {
+
+        var data = PostsSaveRequestDTO.builder()
+                .title(req.get("title").toString())
+                .author(user.getEmail())
+                .content(req.get("content").toString())
+                .build();
+
+        return postsService.save(data);
     }
 
     @PutMapping("/api/v1/posts/{id}")
